df3bd935cfe24863205eaee87a62d31edcbba70f
zpovsic <povsic.zdravko@gmail.com>
2023-01-18 15:48:20 +0100

    Made changes in siteConfig

--- src/lib/siteConfig.ts
+++ src/lib/siteConfig.ts
@@ -1,12 +1,12 @@
-export const SITE_URL = 'https://vercel.zpovsic..app';
+export const SITE_URL = 'https://vercel.zpovsic.MyBlog.app';
 export const APPROVED_POSTERS_GH_USERNAME = ['zpovsic'];
-export const GH_USER_REPO = 'zpovsic/'; // used for pulling github issues and offering comments
-export const SITE_TITLE = 'PZ-Blog';
-export const SITE_DESCRIPTION = "swyx's default SvelteKit + Tailwind starter";
+export const GH_USER_REPO = 'zpovsic/MyBlog'; // used for pulling github issues and offering comments
+export const SITE_TITLE = 'MyBlog';
+export const SITE_DESCRIPTION = "PZ Blog default SvelteKit + Tailwind starter";
 export const DEFAULT_OG_IMAGE =
-	'https://user-images.githubusercontent.com/6764957/147861359-3ad9438f-41d1-47c8-aa05-95c7d18497f0.png';
-export const MY_TWITTER_HANDLE = 'swyx';
-export const MY_YOUTUBE = 'https://youtube.com/swyxTV';
+	'https://avatars.githubusercontent.com/u/1773876?s=40&v=4';
+export const MY_TWITTER_HANDLE = 'zpovsic';
+export const MY_YOUTUBE = 'https://youtube.com/PZ-TV';
 export const POST_CATEGORIES = ['Essay', 'Note']; // Other categories you can consider adding: Talks, Tutorials, Snippets, Podcasts, Notes...
 export const GH_PUBLISHED_TAGS = ['Published'];
 
@@ -17,3 +17,4 @@ export const REPO_OWNER = GH_USER_REPO.split('/')[0];
 // dont forget process.env.GH_TOKEN
 // if supplied, raises rate limit from 60 to 5000
 // https://docs.github.com/en/rest/overview/resources-in-the-rest-api#rate-limiting
+export const GH_TOKEN = process.env.GH_TOKEN;	// !important
